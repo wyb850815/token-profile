dbc527cfb5b4c0baef637d0474c21a432a0ff594
heipacker <heipackermail@gmail.com>
2018-08-23 22:20:43 +0800

    new: add IPOS

--- test/erc20_run.js
+++ test/erc20_run.js
@@ -1,14 +1,15 @@
 let fs = require('fs');
 let path = require('path');
 
-let dirName = "/home/heipacker/StaticProjects/token-profile/erc20_eos";
 /*
+let dirName = "/home/heipacker/StaticProjects/token-profile/erc20_eos";
 fs.readdir(dirName, function (err, files) {
     files.forEach(processFile);
 });
 */
 
-processFile("IPOS.json");
+let dirName = "/home/heipacker/StaticProjects/token-profile/erc20";
+processFile("0x4984270a3723ccfd97cc9710d8cd35b9db10c44d.json");
 
 function processFile(filename) {
     //sample file
@@ -19,9 +20,6 @@ function processFile(filename) {
     let content = fs.readFileSync(fullFileName);
     let bp = JSON.parse(content.toString('utf8').trim());
     if (!bp.symbol) {
-        return
-    }
-    if (!bp.account) {
         return
     }
     if (bp.precision < 0) {
@@ -113,8 +111,8 @@ function processFile(filename) {
         bp.links.medium = "https://medium.com/your-awesome-project"
     }
     let preSql = "INSERT INTO t_token_profile(`blockchain_id`, `overview`, `symbol`, `lang`, `address`, `account`, `email`, `website`, `whitepaper`, `state`, `initial_price_eth`, `initial_price_usd`, `initial_price_btc`, `initial_price_moac`, `initial_price_swtc`, `links_blog`, `links_twitter`, `links_telegram`, `links_github`, `links_facebook`, `links_reddit`, `links_slack`, `links_medium`, `published_on`)";
-    let zhSql = preSql + " VALUES(4, '" + bp.overview.zh + "', '" + bp.symbol + "', 'zh-Hans', '', '" + bp.account + "', '" + bp.email + "', '" + bp.website + "', '" + bp.whitepaper + "', '" + bp.state + "', '" + bp.initial_price.ETH + "', '" + bp.initial_price.USD + "', '" + bp.initial_price.BTC + "', '" + bp.initial_price.MOAC + "', '" + bp.initial_price.SWTC + "', '" + bp.links.blog + "', '" + bp.links.twitter + "', '" + bp.links.telegram + "', '" + bp.links.github + "', '" + bp.links.facebook + "', '" + bp.links.reddit + "', '" + bp.links.slack + "', '" + bp.links.medium + "', '" + bp.published_on + "');";
-    let enSql = preSql + " VALUES(4, '" + bp.overview.en + "', '" + bp.symbol + "', 'en', '', '" + bp.account + "', '" + bp.email + "', '" + bp.website + "', '" + bp.whitepaper + "', '" + bp.state + "', '" + bp.initial_price.ETH + "', '" + bp.initial_price.USD + "', '" + bp.initial_price.BTC + "', '" + bp.initial_price.MOAC + "', '" + bp.initial_price.SWTC + "', '" + bp.links.blog + "', '" + bp.links.twitter + "', '" + bp.links.telegram + "', '" + bp.links.github + "', '" + bp.links.facebook + "', '" + bp.links.reddit + "', '" + bp.links.slack + "', '" + bp.links.medium + "', '" + bp.published_on + "');";
+    let zhSql = preSql + " VALUES(1, '" + bp.overview.zh + "', '" + bp.symbol + "', 'zh-Hans', '', '" + bp.account + "', '" + bp.email + "', '" + bp.website + "', '" + bp.whitepaper + "', '" + bp.state + "', '" + bp.initial_price.ETH + "', '" + bp.initial_price.USD + "', '" + bp.initial_price.BTC + "', '" + bp.initial_price.MOAC + "', '" + bp.initial_price.SWTC + "', '" + bp.links.blog + "', '" + bp.links.twitter + "', '" + bp.links.telegram + "', '" + bp.links.github + "', '" + bp.links.facebook + "', '" + bp.links.reddit + "', '" + bp.links.slack + "', '" + bp.links.medium + "', '" + bp.published_on + "');";
+    let enSql = preSql + " VALUES(1, '" + bp.overview.en + "', '" + bp.symbol + "', 'en', '', '" + bp.account + "', '" + bp.email + "', '" + bp.website + "', '" + bp.whitepaper + "', '" + bp.state + "', '" + bp.initial_price.ETH + "', '" + bp.initial_price.USD + "', '" + bp.initial_price.BTC + "', '" + bp.initial_price.MOAC + "', '" + bp.initial_price.SWTC + "', '" + bp.links.blog + "', '" + bp.links.twitter + "', '" + bp.links.telegram + "', '" + bp.links.github + "', '" + bp.links.facebook + "', '" + bp.links.reddit + "', '" + bp.links.slack + "', '" + bp.links.medium + "', '" + bp.published_on + "');";
     console.log(zhSql);
     console.log(enSql);
 }
